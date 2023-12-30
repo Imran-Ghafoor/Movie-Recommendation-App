c8ddc54fbc541233133d35aacda17abfae4e42a5
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-12-30 23:02:20 +0500

    movie recommendation app project

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,11 +3,13 @@ import Movie from "../Movie/Movie";
 
 const MovieList = ({ movies }) => {
   return (
-    <div className=" p-1 my-2 grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 h-auto ">
-      {movies?.results?.map((movie, index) => (
-        <Movie key={index} movie={movie} index={index} />
-      ))}
-    </div>
+    <main className="mx-auto max-w-[1660px]">
+      <div className="grid h-auto place-content-center gap-10 px-10 py-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {movies?.results?.map((movie, index) => (
+          <Movie key={index} movie={movie} index={index} />
+        ))}
+      </div>
+    </main>
   );
 };
 
